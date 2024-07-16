5702c68740f9bc1659dddb0331c4edfdce4ed863
冷冷 <2270033969@qq.com>
2024-07-16 10:43:31 +0800

    fix： controller 导入日志依赖错误

--- multiple/主子Contoller.java
+++ multiple/主子Contoller.java
@@ -9,7 +9,7 @@ import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 #if($opensource)
 import ${package}.common.core.util.R;
-import ${package}.log.annotation.SysLog;
+import ${package}.common.log.annotation.SysLog;
 import com.pig4cloud.plugin.excel.annotation.ResponseExcel;
 #else
 import ${package}.common.core.util.R;
